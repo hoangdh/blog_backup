d07cdf7286e85b3b3bd7897a6953ec10399c25c3
Dao Huy Hoang <daohuyhoang87@gmail.com>
2017-05-24 00:07:46 +0700

    Update eva

---
+++ crawler_vov2/gen-des.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+list=`ls *.txt`
+
+FOLDER_CUR=`pwd`
+mkdir -p $FOLDER_CUR/hat-ru
+for x in $list
+do
+	title=`cat $x | head -n 1`
+	des=`cat $x | head -n 2`
+	echo -e "KỂ CHUYỆN VÀ HÁT RU CHO BÉ: $title
+
+Chuyên mục: Kể chuyện và hát ru cho bé - VOV2
+
+TÓM TẮT: $des
+
+Nguồn: 
+Chuyên mục: Kể chuyện và hát ru cho bé - VOV2.VOV.VN
+VOV2 - Đài Tiếng Nói Việt Nam
+
+Kênh truyện cổ tích - Kể chuyện và hát ru cho bé là kênh chọn lọc những câu chuyện bổ ích và lý thú sẽ giúp các em có cách nhìn nhận cuộc sống đa màu sắc.Hãy bấm nút đăng ký nhé các em!
+Đăng ký xem miễn phí tại: https://goo.gl/q27eOY" > /$FOLDER_CUR/hat-ru/$x.txt
+done 
---
+++ crawler_vov2/render-v4.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+###########
+#
+# 1. Tai 1 file background
+# 2. Xoa file loi
+# 3. Render kem tieu de va thumb
+#
+#
+###########
+if [ ! -e sub.png ]
+then
+	wget https://github.com/hoangdh/blog_backup/blob/master/crawler_vov2/sub.png?raw=true
+	mv 'sub.png?raw=true' sub.png
+fi
+
+if [ ! -e default-eva.png ]
+then
+	wget https://raw.githubusercontent.com/hoangdh/blog_backup/master/crawler_vov2/default-eva.png
+	mv default-eva.png background.png
+fi
+
+if [ ! -e /opt/ARIAL.TTF ]
+then
+	wget https://raw.githubusercontent.com/hoangdh/blog_backup/master/crawler_vov2/ARIAL.TTF -O /opt/ARIAL.TTF
+fi
+# Xoa nhung file loi
+find . -type f -empty | xargs rm
+
+# Liet ke cac file co trong thu muc
+name=`ls . | egrep '*.mp3'`
+
+for x in $name
+do
+	x=`basename $x .mp3`
+    echo $x
+	
+	# Lay title
+	title=`cat $x.txt | head -n 1 | tr ':' ' '`
+	
+    ### Chen text vao background
+	ffmpeg -i background.jpg -vf "drawtext="fontfile=/opt/ARIAL.TTF": text='$title': fontcolor=white: fontsize=30: box=1: boxcolor=black@0.5: boxborderw=5: x=40: y=20" $x.png
+	
+	### Sua thumbnail
+	ls $x.jpg
+	if [ "$?" == "2" ]
+	then
+		ffmpeg -loop 1 -i $x.png -i $x.mp3 -c:v libx264 -aspect 16:9 -s 1280x720 -preset veryfast  -c:a aac -strict experimental -b:a 192k -shortest $x.mp4
+	else
+		ffmpeg -i $x.jpg -s 400x300 -vf scale=400:300 $x-thumb.png
+		### Render video 720p
+		ffmpeg -loop 1 -i $x.png -i $x.mp3 -i $x-thumb.png -filter_complex "overlay=75:75"  -c:v libx264 -aspect 16:9 -s 1280x720 -preset veryfast  -c:a aac -strict experimental -b:a 192k -shortest $x.mp4
+	fi
+done 
